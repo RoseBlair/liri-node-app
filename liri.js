7ab683099b20304b03879e17036f991bb57639e8
Carolyn Bernier <carolynbbernier@gmail.com>
2018-10-22 23:30:07 -0400

    worked on omdb

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 var request = require('request');
-var spotify = require("./keys.js");
+var keys = require("./keys.js");
 require("moment");
 
 
@@ -32,16 +32,52 @@ switch (input) {
         callConcert(artist);
         break;
     case "spotify-this-song":
-        console.log("fucking song");
+        console.log("song");
         break;
     case "movie-this":
-        console.log("fucking movie");
+        var moTitle = (process.argv[3]);
+        callOmdb(moTitle);
         break;
     case "do-what-it-says":
-        console.log("fucking do it");
+        console.log("do it");
         break;
     default:
         console.log("Doesn't work")
        
 
     };
+
+    
+
+    function callOmdb (moTitle) {
+        request("http://www.omdbapi.com/?apikey=" + keys.omdb.id + "&t=" + moTitle, function (error, response, body) {
+            var body = JSON.parse(body);
+            if(error){
+                console.log('error:', error); // Print the error if one occurred
+            }
+            
+            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                    var title = body.Title;
+                    var year = body.year;
+                    var ratings1 = body.ratings[0];
+                    var ratings2 = body.ratings[1];
+                    var country = body.country;
+                    var actors = body.Actors;
+                    var plot = body.Plot;
+                    var language = body.language;
+                    var movieInfo = "title: " + title + "\n" +
+                        "year: " + year + "\n" +
+                        "IMDB rating: " + ratings1 + "\n" +
+                        "Rotten Tomato rating: " + ratings2 + "\n" +
+                        "country: " + country + "\n" +
+                        "actors: " + actors + "\n" +
+                        "plot: " + plot + "\n" +
+                        "language: " + language + "\n" +
+                        
+                 console.log();
+             console.log('body:', body); // Print the HTML for the Google homepage.
+    
+        });
+    }
+
+    
